c17ef4cd87348ed55318253967074ee4afcd6d9c
Radoslaw Skupnik <r.skupnik@gmail.com>
2021-02-19 20:28:40 +0100

    Basic setup

---
+++ src/main/java/com/github/rskupnik/springmetricsexample/SpringMetricsExampleApplication.java
@@ -0,0 +1,13 @@
+package com.github.rskupnik.springmetricsexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMetricsExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMetricsExampleApplication.class, args);
+	}
+
+}
